113cff7414358f934a14c9ad35bb9ec13e146792
curlink19 <p.kurochkin200@gmail.com>
2023-08-19 14:12:47 +0300

    data_generator

--- data_generator.py
+++ data_generator.py
@@ -1,8 +1,12 @@
 import torch
 
+
 class TableDataGenerator:
     def __init__(self, generator):
         self.generator = generator
 
-    def get(self, size):
-        return torch.from_numpy(self.generator(size)).to(torch.float) 
+    def get(self, size=1):
+        return [
+            torch.from_numpy(self.generator()).to(torch.float)
+            for i in range(size)
+        ]
